9903517590d65c28fd805f3a2ab3bae3b4bb94b9
marsonshine <ms27946@outlook.com>
2020-04-01 15:39:03 +0800

    feat: 添加 mvc client

---
+++ MVCClient/Config/Configs.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCClient.Config
+{
+    public class Configs
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    // 登录成功回调处理地址，处理回调返回的数据
+                    RedirectUris = { "http://localhost:5004/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5004/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+        }
+    }
+}
