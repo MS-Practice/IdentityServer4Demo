dc823fe34daa36319c9d4133944580e99c2945c4
marsonshine <ms27946@outlook.com>
2020-05-26 18:28:37 +0800

    fix: 自定义授权验证

---
+++ AuthServer/Services/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// https://stackoverflow.com/questions/35304038/identityserver4-register-userservice-and-get-users-from-database-in-asp-net-core/35306021#35306021
+namespace AuthServer.Services
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
